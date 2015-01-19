4de793dd41e8b631f6d83555ae5d3c836312b9a3
Christoph Dorn <christoph@christophdorn.com>
2015-01-18 20:34:17 -0800

    start more directly

--- scripts/start.js
+++ scripts/start.js
@@ -11,8 +11,15 @@ rm('-Rf', [
 	'../harviewer/fireconsole/.rt'
 ]);
 
-cd('server');
-if (exec('node server.js').code !== 0) {
-	echo('Error: command failed');
-	exit(1);
-}
+/*
+POLICY: Invocation of runtime should be done as direct as possible to avoid unnecessary processes.
+*/
+	require("../server/node_modules/pinf-for-nodejs").main(require("../server/server").main, module);
+/*
+	i.e. not like this:
+	cd('server');
+	if (exec('node server.js').code !== 0) {
+		echo('Error: command failed');
+		exit(1);
+	}
+*/
