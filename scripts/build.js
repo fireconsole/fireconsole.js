0b6f781208095c37d633b513d94efc5af759e660
Christoph Dorn <christoph@christophdorn.com>
2015-04-23 10:50:11 -0700

    stop build for now

--- scripts/build.js
+++ scripts/build.js
@@ -7,17 +7,23 @@ cd(__dirname + '/..');
 
 rm('-Rf', 'demo/.rt');
 cd('demo');
+console.log("TODO: Fixme: " + new Error().stack);
+/*
 if (exec('../server/node_modules/.bin/pinf bundle').code !== 0) {
 	echo('Error: command failed');
 	exit(1);
 }
+*/
 cd('..');
 
 
 rm('-Rf', 'tests/.rt');
 cd('tests');
+console.log("TODO: Fixme: " + new Error().stack);
+/*
 if (exec('../server/node_modules/.bin/pinf bundle').code !== 0) {
 	echo('Error: command failed');
 	exit(1);
 }
+*/
 cd('..');
