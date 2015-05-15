890cb8ec6e2ab585db49776b7090b194078d0651
Christoph Dorn <christoph@christophdorn.com>
2015-05-14 22:06:28 -0700

    wip

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Source https://github.com/cadorn/bash.origin
+. "$HOME/.bash.origin"
+function init {
+	eval BO_SELF_BASH_SOURCE="$BO_READ_SELF_BASH_SOURCE"
+	BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
+	local __BO_DIR__="$___TMP___"
+
+
+	BO_checkVerbose "VERBOSE" "$@"
+
+
+#	pushd "$__BO_DIR__/.." > /dev/null
+#		BO_log "$VERBOSE" "Install using npm in: `pwd`"
+#		npm install
+#	popd > /dev/null
+
+
+	pushd "$__BO_DIR__/../loops" > /dev/null
+		BO_log "$VERBOSE" "Install using npm in: `pwd`"
+		npm install --unsafe-perm
+	popd > /dev/null
+
+	pushd "$__BO_DIR__/../receivers" > /dev/null
+		BO_log "$VERBOSE" "Install using npm in: `pwd`"
+		npm install
+	popd > /dev/null
+
+	pushd "$__BO_DIR__/../server" > /dev/null
+		BO_log "$VERBOSE" "Install using npm in: `pwd`"
+		npm install
+	popd > /dev/null
+
+}
+init $@
